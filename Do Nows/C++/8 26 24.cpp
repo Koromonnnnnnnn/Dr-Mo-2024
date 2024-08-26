5b4ae201df6a0f576bb0c5d38979ce0db90f5174
Koromonnnnnnnn <764363@dpsk12.net>
2024-08-26 09:13:45 -0600

    Initial commit

---
+++ Do Nows/C++/8 26 24.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int j = 70; j <= 80; j += 1)
+        cout << j << " ";
+
+    cout << endl;
+
+    for (int k = 100; k <= 300; k += 20)
+        cout << k << " ";
+
+    cout << endl;
+
+    for (int i = 25; i >= -25; i -= 5)
+        cout << i << " ";
+
+    cout << endl;
+
+    for (int i = 10000; i >= 10; i /= 5)
+        cout << i << " ";
+
+    cout << endl;
+}
---
+++ Do Nows/Python/8 22 24.py
@@ -0,0 +1,2 @@
+print("What did the horse say after it tripped?")
+print("Help! I’ve fallen and I can’t giddyup!")
