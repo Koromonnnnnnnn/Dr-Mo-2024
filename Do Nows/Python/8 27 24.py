0b2f20e474d850190909936454b34338b2868c30
Koromonnnnnnnn <764363@dpsk12.net>
2024-08-27 09:11:56 -0600

    Create 8 27 24.py

---
+++ Do Nows/Python/8 27 24.py
@@ -0,0 +1,24 @@
+# ------------------------------------
+
+choice = " "
+while choice != "yes":
+    print(choice)
+    choice = input("Would you like fish?")
+
+print("fish")
+
+# ------------------------------------
+
+choice = " "
+while choice != "7":
+    print(choice)
+    choice = input("Guess a number 1-10")
+
+print("correct")
+
+# ------------------------------------
+
+num = 0
+while num <= 20:
+    print(num, end=" ")
+    num += 2
