fcc4ab53e30addbc208c0cd2f4c63a712c1e015e
Koromonnnnnnnn <764363@dpsk12.net>
2024-09-09 09:50:20 -0600

    .

---
+++ Minor Assignments/9 9 24.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int j = 0; j <= 18; j++) // 0-18
+        cout << j << " ";
+
+    cout << endl;
+
+    for (int k = 25; k <= 45; k++) // 25-45
+        cout << k << " ";
+
+    cout << endl;
+
+    for (int i = 40; i <= 80; i += 4) // 40-80 by 4's
+        cout << i << " ";
+
+    cout << endl;
+
+    for (int i = 12; i >= -6; i -= 3) // 12--6 by -3's
+        cout << i << " ";
+
+    cout << endl;
+}
