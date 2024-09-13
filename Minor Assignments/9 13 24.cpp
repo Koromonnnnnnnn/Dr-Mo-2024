4e69b0d070ad4db6b2e495fe5eeb140014e4b0ea
Koromonnnnnnnn <764363@dpsk12.net>
2024-09-13 09:57:53 -0600

    .

---
+++ Minor Assignments/9 13 24.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool pointCollision(int x1, int y1, int x2, int y2)
+{
+    return (x1 == x2) && (y1 == y2);
+}
+
+int main()
+{
+
+    int x1, y1;
+    int x2, y2;
+
+    cout << "Please enter x1:" << endl;
+    cin >> x1;
+    cout << "Please enter y1:" << endl;
+    cin >> y1;
+    cout << "Please enter x2:" << endl;
+    cin >> x2;
+    cout << "Please enter y2:" << endl;
+    cin >> y2;
+
+    if (pointCollision(x1, y1, x2, y2))
+    {
+        cout << "The points collide." << endl;
+    }
+    else
+    {
+        cout << "The points do not collide." << endl;
+    }
+
+    return 0;
+}
---
+++ Minor Assignments/9 13 24.py
@@ -0,0 +1,10 @@
+numOne = int(input("Input number 1: "))
+numTwo = int(input("Input number 2: "))
+numThree = int(input("Input number 3: "))
+
+
+def average(num1, num2, num3):
+    print((num1 + num2 + num3) / 3)
+
+
+average(numOne, numTwo, numThree)
