b1a40a9a06897f9c0063ab5c62528e55f94ebdfb
Koromonnnnnnnn <764363@dpsk12.net>
2024-10-22 09:58:47 -0600

    .

--- Do Nows/C++/10 22 24.cpp
+++ Do Nows/C++/10 22 24.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
-int factorialCalculator(int n){
-    if (n<=1){
-        
-    }
+int factorialCalculator(int n)
+{
+
+    return n * factorialCalculator(n - 1);
 }
 
-int main(){
+int main()
+{
 
+    int c = factorialCalculator(5);
+    cout << c;
 }
