604b77e41dcbda51cf963193523e1db65cb86354
Koromonnnnnnnn <764363@dpsk12.net>
2024-10-22 10:25:02 -0600

    Update 10 22 24.cpp

--- Do Nows/C++/10 22 24.cpp
+++ Do Nows/C++/10 22 24.cpp
@@ -2,15 +2,41 @@
 #include <cmath>
 using namespace std;
 
-int factorialCalculator(int n)
-{
+// Broken Spicy Code attempt
+// int factorialCalculator(int n) // Broken Spicy Code attempt
+//{
+// return n * factorialCalculator(n - 1);
+//}
 
-    return n * factorialCalculator(n - 1);
+double area(double length, double width)
+{
+    return length * width;
+}
+
+bool isPrime(int n)
+{
+    int num = n-1;
+    while (num > 1)
+    {
+        if (n % num == 0)
+        {
+            cout << num <<  " divided evenly" << endl;
+            return false;
+            break;
+        }
+        else
+        {
+            cout << "didn't divide evenly" << endl;
+            break;
+        }
+        num--;
+    }
+    return true;
 }
 
 int main()
 {
 
-    int c = factorialCalculator(5);
-    cout << c;
+    cout << area(5, 2) << endl;
+    cout << isPrime(13);
 }
