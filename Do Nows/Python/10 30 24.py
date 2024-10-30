0723b6f86c08c5e887c9c1335432351b54e06af3
Koromonnnnnnnn <764363@dpsk12.net>
2024-10-30 09:46:30 -0600

    .

--- Do Nows/Python/10 30 24.py
+++ Do Nows/Python/10 30 24.py
@@ -1,4 +1,11 @@
 import numpy as np
+import matplot.pyplot as plt
 
-matrix = np.random.randint(1,101, size=(4, 4))
+matrix = np.random.randint(1, 101, size=(4, 4))
 print("Generated Matric:\n", matrix)
+
+max_value = np.max(matrix)
+print("Maximum Value:", max_value)
+
+min_value = np.min(matrix)
+print("Minimum Value:", min_value)
