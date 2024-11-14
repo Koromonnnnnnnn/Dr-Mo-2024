e2aad0b5ac49946f9168bb40b9521bec7a70aa2f
Koromonnnnnnnn <764363@dpsk12.net>
2024-11-14 10:06:41 -0700

    .

--- Do Nows/Python/11 14 24.py
+++ Do Nows/Python/11 14 24.py
@@ -1,11 +1,12 @@
-enoch = [1,2,3,4,5]
+enoch = [3, 2, 1, 4, 5]
 temp = 0
 listSize = len(enoch)
 
-for i in range(listSize):
-    if i > enoch[i+1]:
-        temp = enoch[i]
-        enoch[i] = enoch[i + 1]
-        enoch[i + 1] = temp
+for j in range(listSize - 1):
+    for i in range(listSize - 1):
+        if enoch[i] > enoch[i + 1]:
+            temp = enoch[i]
+            enoch[i] = enoch[i + 1]
+            enoch[i + 1] = temp
 
 print(enoch)
