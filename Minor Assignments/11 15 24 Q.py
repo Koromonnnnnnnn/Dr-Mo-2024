2aa15b59d7175658ca1f7fd7efa604c893807b4c
Koromonnnnnnnn <764363@dpsk12.net>
2024-11-15 08:59:22 -0700

    Create 11 15 24 Q.py

---
+++ Minor Assignments/11 15 24 Q.py
@@ -0,0 +1,21 @@
+zoo = ["Monkey", "Elephant", "Lion", "Tiger", "Giraffe"]
+
+print(zoo)
+
+zoo.append("Panda")
+
+print(zoo)
+
+sizeList = len(zoo)
+
+print("There are about", sizeList, "animals in the zoo.")
+
+print("A lion escaped oh no")
+
+zoo.remove("Lion")
+
+print(zoo)
+
+zoo[1] = "Meerkat"
+
+print(zoo)
