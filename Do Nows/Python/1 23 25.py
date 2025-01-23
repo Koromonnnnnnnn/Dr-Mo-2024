f3d9fa75ee7fb460d94f48f186fb06ed206b3531
Koromonnnnnnnn <764363@dpsk12.net>
2025-01-23 08:39:17 -0700

    Create 1 23 25.py

---
+++ Do Nows/Python/1 23 25.py
@@ -0,0 +1,10 @@
+
+arr =  [2, 3, 4, 10, 40]
+x = 10
+
+result = binarySearch(arr, 0, len(arr)-1, x)
+
+if result != -1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present in array")
