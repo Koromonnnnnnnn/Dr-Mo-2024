ed6856224f7dd1156aabc74a2b40838885b982b0
Koromonnnnnnnn <764363@dpsk12.net>
2025-01-23 08:44:18 -0700

    Update 1 23 25.py

--- Do Nows/Python/1 23 25.py
+++ Do Nows/Python/1 23 25.py
@@ -1,9 +1,18 @@
+def binarySearch(arr, low, high, x):
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
 
-
-arr =  [2, 3, 4, 10, 40]
+arr = [2, 3, 4, 10, 40]
 x = 10
 
-result = binarySearch(arr, 0, len(arr)-1, x)
+result = binarySearch(arr, 0, len(arr) - 1, x)
 
 if result != -1:
     print("Element is present at index", result)
