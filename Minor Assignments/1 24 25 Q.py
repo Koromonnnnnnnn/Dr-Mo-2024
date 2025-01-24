3784cb2ad072f73b1ab3894aeaf0234d2b088f85
Koromonnnnnnnn <764363@dpsk12.net>
2025-01-24 09:04:09 -0700

    Update 1 24 25 Q.py

--- Minor Assignments/1 24 25 Q.py
+++ Minor Assignments/1 24 25 Q.py
@@ -1,10 +1,10 @@
 #Python Quiz 1/24/2025
 
 def sumDigits(x):
-    if x == 0:
+    if x == 0: #Because zero is zero
         return 0
 
-    n = x - (x // 10) * 10
+    n = x - (x // 10) * 10 #Get last digit of the number
     return n + sumDigits(x // 10)
 
 
