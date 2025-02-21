c7ec62d809ed1e5a2783e09a47979153a61e2342
Koromonnnnnnnn <764363@dpsk12.net>
2025-02-21 09:13:12 -0700

    .

--- Major Assignments/Valentine.py
+++ Major Assignments/Valentine.py
@@ -4,48 +4,69 @@ import pygame
 pygame.init()
 pygame.display.set_caption("Valentine's Day Card")
 screen = pygame.display.set_mode((800, 800))
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.Font("freesansbold.ttf", 32)
 img = pygame.image.load("dog.jpg")
+
+RED = (250, 0, 0)
+ORANGE = (200, 100, 0)
+GREEN = (0, 150, 0)
+
 
 class Heart:
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
-    
+
     def draw(self, surface):
         left_circle_center = (self.x - 20, self.y)
         right_circle_center = (self.x + 20, self.y)
-        triangle_points = [(self.x - 40, self.y + 5),
-                           (self.x + 40, self.y + 5),
-                           (self.x, self.y + 50)]
-        
+        triangle_points = [
+            (self.x - 40, self.y + 5),
+            (self.x + 40, self.y + 5),
+            (self.x, self.y + 50),
+        ]
+
         pygame.draw.circle(surface, self.color, left_circle_center, 20)
         pygame.draw.circle(surface, self.color, right_circle_center, 20)
         pygame.draw.polygon(surface, self.color, triangle_points)
 
+
 class Flower:
-    def __init__(self, x, y, color):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.color = color
-    
-    def draw(self, surface):
-        left_circle_center = (self.x - 20, self.y)
-        right_circle_center = (self.x + 20, self.y)
-        triangle_points = [(self.x - 40, self.y + 5),
-                           (self.x + 40, self.y + 5),
-                           (self.x, self.y + 50)]
-        
-    pygame.draw.circle(surface, (250,0,0), (180, 200), 20) #top left circle
-    pygame.draw.circle(surface, (250,0,0), (220, 200), 20) #top right circle
-    pygame.draw.polygon(surface, (250,0,0), ((160, 205),(239, 205), (200, 250))) #triangle to form base
+
+    def draw(self, screen):
+        # Draw the stem
+        pygame.draw.rect(screen, GREEN, (self.x - 10, self.y, 20, 100))
+
+        # Draw the petals
+        pygame.draw.circle(screen, RED, (self.x - 20, self.y), 20)  # top-left petal
+        pygame.draw.circle(
+            screen, RED, (self.x - 20, self.y - 40), 20
+        )  # bottom-left petal
+        pygame.draw.circle(screen, RED, (self.x + 20, self.y), 20)  # top-right petal
+        pygame.draw.circle(
+            screen, RED, (self.x + 20, self.y - 40), 20
+        )  # bottom-right petal
+
+        # Draw the center of the flower
+        pygame.draw.circle(screen, ORANGE, (self.x, self.y - 20), 20)  # flower center
+
 
 # Create instances of Heart
-heart1 = Heart(50, 200, (250, 0, 0))
-heart2 = Heart(250, 200, (250, 0, 0))  # You can ask students to change positions and colors
-heart3 = Heart(450, 200, (250, 0, 0))
-heart4 = Heart(650, 200, (250, 0, 0)) 
+heart1 = Heart(50, 100, (250, 0, 0))
+heart2 = Heart(
+    250, 100, (250, 0, 0)
+)  # You can ask students to change positions and colors
+heart3 = Heart(450, 100, (250, 0, 0))
+heart4 = Heart(650, 100, (250, 0, 0))
+
+flower1 = Flower(50, 220)
+flower2 = Flower(250, 220)
+flower3 = Flower(450, 220)
+flower4 = Flower(650, 220)
 
 # Draw everything
 heart1.draw(screen)
@@ -58,8 +79,8 @@ flower2.draw(screen)
 flower3.draw(screen)
 flower4.draw(screen)
 
-text1 = font.render('I Love You!', True, (250, 100, 100))
-text2 = font.render('Happy Valentines Day', True, (250, 0, 0), (200, 150, 150))
+text1 = font.render("I Love You!", True, (255, 255, 255))
+text2 = font.render("Happy Valentines Day", True, (250, 0, 0), (200, 150, 150))
 screen.blit(text1, (200, 100))
 screen.blit(text2, (400, 300))
 screen.blit(img, (0, 400))
