8b2eb33c5908c020cf85453787a21e402e405a9e
Koromonnnnnnnn <764363@dpsk12.net>
2025-03-04 08:58:35 -0700

    Create astro.py

---
+++ Do Nows/Python/astro.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+
+from astropy.utils.data import download_file
+
+from astropy.visualization import LogStretch, PowerStretch
+
+from astropy.visualization.mpl_normalize import ImageNormalize
+
+image_file = download_file(
+    "http://data.astropy.org/tutorials/FITS-images/HorseHead.fits", cache=True
+)
+image_data = fits.getdata(image_file)
+
+import pprint
+
+header = fits.getheader(image_file)
+pprint.pprint(header)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image_data, cmap="cividis")
+plt.colorbar()
+plt.show()
+
+print("Min:", np.min(image_data))
+print("Max:", np.max(image_data))
+print("Mean:", np.mean(image_data))
+print("Stdev", np.std(image_data))
