5ccdb9612e686c484f814c6cf0f051b2b4b57ebc
Koromonnnnnnnn <764363@dpsk12.net>
2025-03-07 09:28:57 -0700

    .

--- Minor Assignments/3 7 25.py
+++ Minor Assignments/3 7 25.py
@@ -2,25 +2,29 @@ def calculate_circumference(r):
     C = 2.0 * (3.14 * r)
     return C
 
+
 print(calculate_circumference(5))
 
+
 def zigzag_pattern(n):
-  for i in range(1, n, 2):
-    print(i, end=" ")
-  
-  for i in range(i - 2, 0, -2): 
-    print(i, end=" ")
+    for i in range(1, n, 2):
+        print(i, end=" ")
+
+    for i in range(i - 2, 0, -2):
+        print(i, end=" ")
+
 
 zigzag_pattern(10)
 
+
 def sumtreefive(n):
-  sum = 0
-  for i in range(1, n + 1):
-    if i % 3 == 0 or i % 5 == 0:
-      sum += i
-      print(i)
-  return sum
-  
-print(sumtreefive(10))
-print(sumtreefive(20))
-print(sumtreefive(100))
+    sum = 0
+    for i in range(1, n + 1):
+        if i % 3 == 0 or i % 5 == 0 and i <= n:
+            sum += i
+    return sum
+
+
+print(sumtreefive(14))
+print(sumtreefive(36))
+print(sumtreefive(22))
