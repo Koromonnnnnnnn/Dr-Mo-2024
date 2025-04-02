d80628691e2b03399a4c71729ab3c88623495e80
Koromonnnnnnnn <764363@dpsk12.net>
2025-04-02 09:37:26 -0600

    Create 4 2 25.cpp

---
+++ Do Nows/C++/4 2 25.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int note;
+    cout << "type 1 for a low note, 2 for a medium note, and 3 for a high note." << endl;
+    cin >> note;
+    if (note <= 1)
+        Beep(100, 500);
+    else if (note <= 2)
+        Beep(200, 500);
+    else
+        Beep(300, 500);
+}
