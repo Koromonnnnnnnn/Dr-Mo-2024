86a52b02051dbfc6ee3379db0dee95f81880b189
Koromonnnnnnnn <764363@dpsk12.net>
2025-04-10 09:54:40 -0600

    Update tictactoe.cpp

--- Minor Assignments/tictactoe.cpp
+++ Minor Assignments/tictactoe.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void printBoard(const char board[3][3]);
+
 int main()
 {
 
@@ -12,10 +14,23 @@ int main()
 
     while (endGame == false)
     {
+        printBoard(board);
         cout << "Player" << player << "'s turn!" << endl
              << endl;
 
         if (player == 'X')
             player = 'Y';
+    }
+}
+
+void printBoard(const char board[3][3])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
     }
 }
